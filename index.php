06c867a9f67a6bf1cc91009e443c47da57330e7b
abdllhblc <92243276+abdllhblc@users.noreply.github.com>
2022-11-03 22:07:50 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet"  href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+	<title>İndex</title>
+
+
+	<style type="text/css">
+                body{
+                	background: rgb(52, 73, 94);
+                   }
+				h1{
+					position:relative;
+					left:530px;
+					display:inline;
+					
+					}
+			.btn{
+				display:inline;
+				position:relative;
+				left:625px;
+				top:275px;
+			}
+			   
+			   .btn-warning{
+			   	width:170px;
+			   	height:70px;
+			   }
+			a{
+			    text-decoration:none;
+				color:black;
+				font-size:20px;
+			}
+
+	</style>
+
+
+</head>
+<body>
+		<div class="text-bg-primary p-3"><h1>ABDULLAH BALCI</h1></div>
+		<div class="btn-div"><button type="button" class="btn btn-warning"><a href="rehber.php">REHBER</a></button></div>
+       
+ 
+
+
+
+		
+
+</body>
+</html>
